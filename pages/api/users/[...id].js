363570c6376ab2d9247fdf47808e06c99f57bf35
alwaystaegi <ffff4565@naver.com>
2022-09-23 14:40:12 +0900

    도메인의 주소를 통해서 여러 인자를 받는 방법=> [...id].js처럼 파일명을 대괄호로 감싸고 그 안에 받을 변수의 이름을 쓴다. ...을 빼면 한개의 인자만 받게 된다.

---
+++ pages/api/users/[...id].js
@@ -0,0 +1,12 @@
+//!api 폴더: 서버를 위한 폴더(이름 자체만으로 next에서 서버내의 파일로 인식함)
+
+export default function handler(
+  req,
+  res /*//!req res== props와 같은 인자지만 받는 요소가 다름 */
+) {
+  console.log(req.query.id[0]);
+  console.log(req.query.id[1]);
+  //
+
+  res.status(500).json({ ok: true, message: "GET CALL" });
+}
